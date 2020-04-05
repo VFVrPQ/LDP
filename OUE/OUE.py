66dc85025c11e98d983f60e54cca36a37684820f
hyylc <1959232004@qq.com>
2020-04-05 11:06:23 +0800

    OUE empirical_eps不太对

--- OUE/OUE.py
+++ OUE/OUE.py
@@ -1,55 +1,53 @@
 import math
 import random
 
-class SUE:
-    """Direct Encoding"""
+class OUE:
     def __init__(self, d, epsilon, n):
         self.__d = d # input size
         self.__epsilon = epsilon # privacy budget
-        self.__p = math.exp(epsilon/2) / (math.exp(epsilon/2)+1) # probability of pertubation into itself
-        self.__q = 1.0 / (math.exp(epsilon/2)+1) # probability of pertubation into xxx
+        self.__p = 1.0 / 2 # probability of pertubation into itself
+        self.__q = 1.0 / (math.exp(epsilon) + 1) # probability of pertubation into xxx
         self.__counterPert = d*[0] # count perturbed number
         self.__counterReal = d*[0] # count real number
+        self.__counterEsti = d*[0] # ~c(i)
         self.__n = n # the number of users
     
     # Encode(x) = x
-    def __encoding(self, d, x):
-        ret = d*[0]
+    def __encoding(self, x):
+        ret = self.__d*[0]
         ret[x-1] = 1
         self.__counterReal[x-1] += 1 # counter
         return ret
 
-    def __perturbing(self, d, x):
-        ret = self.__random_pick(d, x)
+    def __perturbing(self, x):
+        ret = self.__random_pick(self.__d, x)
         #ret -= 1 # corresponding to index
-        for i in range(d):
+        for i in range(self.__d):
             self.__counterPert[i] += ret[i]
         return ret
 
-    def PE(self, d, x):
-        e = self.__encoding(d, x)
-        pe = self.__perturbing(d, e)
+    def PE(self, x):
+        e = self.__encoding(x)
+        pe = self.__perturbing(e)
         return pe
 
     def aggregation(self):
-        self.__counterEsti = self.__d*[0]
-
         # for convience
-        eps = self.__epsilon
         d = self.__d
         n = self.__n
+        p = self.__p
+        q = self.__q     
         for i in range(d):
-            self.__counterEsti[i] = (self.__counterPert[i]*(math.exp(eps/2)+1)-n)/(math.exp(eps/2)-1)
-        return self.__counterReal, self.__counterPert, self.__counterEsti
+            self.__counterEsti[i] = (self.__counterPert[i]-n*q)/(p-q)
 
     # numerical/analytical value of variance
     def var_analytical(self):
-        e = math.exp(self.__epsilon/2)
+        e = math.exp(self.__epsilon)
         if e==1:
             print('var analytical error : e=', 1)
             return -1
         n = self.__n
-        return n*e/(e-1)/(e-1)
+        return n*4*e/(e-1)/(e-1)
 
     # empirical value of variance
     def var_empirical(self, f):
@@ -65,9 +63,9 @@ class SUE:
         self.__n = n
 
     # input number v
-    def __random_pick(self, d, v): 
+    def __random_pick(self, d, v):
         for i in range(d):
-            x = random.uniform(0, 1)
-            if x > self.__p: # Pr[0, p] pertubated into itself
+            x = random.uniform(0, self.__p+self.__q)
+            if x > self.__p : # Pr[0, p] pertubated into itself
                 v[i] = 1 - v[i]
         return v
--- OUE_test.py
+++ OUE_test.py
@@ -1,5 +1,6 @@
-from SUE.SUE import SUE
+from OUE.OUE import OUE
 from Components.Draw import draw
+from Components.Distribution import zipf
 import math
 import random
 
@@ -23,22 +24,19 @@ def analytical_eps(protocol, epsilon, n):
         vars.append(x.var_analytical())
     return vars
 
-def empirical_eps(protocol, epislon, n):
+def empirical_eps(protocol, epsilon, n):
     vars = []
-    for step in range(7): # 9 values -> 7values(对应修改varslist)
+    for step in range(7): # 9 values
         d = 2**(2 + step*2)
-        #d = 2**7
-        x = protocol(d, epislon, n)
-        f = []
+        x = protocol(d, epsilon, n)
+        users = zipf.zipf(1.1, d, n)
         for i in range(n): # random PE
-            print(i,step)
+            if i%1000 == 0:
+                print(i,step)
             #f.append(1.0/d)
-            m = random.randint(1,d)
-            #print(m)
-            #print(' ')
-            x.PE(d,m)
-        for i in range(d):
-            f.append(1.0/d)
+            x.PE(users[i])
+
+        f = zipf.probList(1.1, d, n)
         x.aggregation()
         vars.append(x.var_empirical(f))
     return vars
@@ -47,31 +45,31 @@ def empirical_eps(protocol, epislon, n):
 def numerical_values_of_var2(): 
     varslist = [] # return a list of (a list of variances)
     d=2**10 # 2^10
-    vars = analytical(SUE, d, 10000)
+    vars = analytical(OUE, d, 10000)
     for i in range(len(vars)):
         vars[i] = math.log(vars[i]) / math.log(10)
     varslist.append(vars)
     epss = [0.5+0.5*item for item in range(10)] # 0.5, 1.0, ..., 5.0
-    draw.lines(epss, varslist, ['SUE'], ylabel='Var(log10(y))')
+    draw.lines(epss, varslist, ['OUE'], ylabel='Var(log10(y))')
 
 # Figure 2(a)
 def Comparing_empirical_and_analytical_variance1():
     varslist = [] # return a list of (a list of variances)
     # Analytical DE
-    vars = analytical_eps(SUE, 4, 10000)
+    vars = analytical_eps(OUE, 4, 10000)
     for i in range(len(vars)): #log10
         vars[i] = math.log(vars[i]) / math.log(10)
     varslist.append(vars)
     # Empirical DE
     
-    vars = empirical_eps(SUE, 4, 10000)
+    vars = empirical_eps(OUE, 4, 10000)
     for i in range(len(vars)): #log10
         vars[i] = math.log(vars[i]) / math.log(10)
     varslist.append(vars)
     
     # Draw
-    d = [item for item in range(2, 20, 2)] # 2^2, 2^4, ..., 2^18
-    draw.lines(d, varslist, ['Analytical SUE', 'Empirical SUE'], xlabel='Vary d(log2(d))', ylabel='Vary n(log10(n))')
+    d = [item for item in range(2, 16, 2)] # 2^2, 2^4, ..., 2^18
+    draw.lines(d, varslist, ['Analytical OUE', 'Empirical OUE'], xlabel='Vary d(log2(d))', ylabel='Vary n(log10(n))')
 
-numerical_values_of_var2()
-#Comparing_empirical_and_analytical_variance1()
+#numerical_values_of_var2()
+Comparing_empirical_and_analytical_variance1()
